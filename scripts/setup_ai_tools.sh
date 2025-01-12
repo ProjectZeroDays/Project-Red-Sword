de77bd11b489ea40121d3823af89ffa055b561e5
PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>
2025-01-12 15:30:37 -0600

    Delete scripts/setup_ai_tools.sh
    Signed-off-by: PROJECT ZERO <56379955+ProjectZeroDays@users.noreply.github.com>

--- scripts/setup_ai_tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup AI tools and APIs
-echo "Setting up AI tools..."
-
-# Install necessary Python packages
-pip install gradio aiohttp
-pip install transformers
-pip install panel
